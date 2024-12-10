aed03a9e2e6ebda0f9e7e789db66ed5b32d25812
Yatin Vohra <96340665+vohraaYatinn@users.noreply.github.com>
2024-12-10 11:08:54 +0530

    okk

--- src/components/common/LoginModal.jsx
+++ src/components/common/LoginModal.jsx
@@ -63,7 +63,7 @@ const LoginModal = () => {
   
     // Prepare email data
     const emailData = {
-      to: [{ email: "connectwithyatin@gmail.com" }], // Replace with the recipient's email address
+      to: [{ email: "ankitasingh182000@gmail.com" }], // Replace with the recipient's email address
       subject : `ATS new Leads - ${formattedDate}`,
       textContent: "This is the plain text content of your email.",
       htmlContent: `
