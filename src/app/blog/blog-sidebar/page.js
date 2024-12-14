842457bb07fda53173e880bf51e398c66c0005cd
Rakshit Tiwari <59415104+tiwarirakshit@users.noreply.github.com>
2024-12-14 21:11:07 +0530

    update blog sidebar page metadata for branding alignment

--- src/app/blog/blog-sidebar/page.js
+++ src/app/blog/blog-sidebar/page.js
@@ -6,9 +6,9 @@ import Topbar from "@/components/topbar/Topbar";
 import Link from "next/link";
 
 export const metadata = {
-  title: "TripRex - Tour & Travel Agency  NextJs Template",
+  title: "ATS - tour & travel",
   description:
-    "TripRex is a NextJs Template for Tour and Travel Agency purpose",
+    "ATS is a Tour and Travel Agency",
   icons: {
     icon: "/assets/img/sm-logo.svg",
   },
