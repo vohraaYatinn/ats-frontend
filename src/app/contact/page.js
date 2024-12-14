7fbaa933f27372698d20a06bc00b4251b20aafb5
Rakshit Tiwari <59415104+tiwarirakshit@users.noreply.github.com>
2024-12-14 21:11:35 +0530

    refactor contact page layout and styles for improved design and responsiveness

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,13 +1,13 @@
 import React from "react";
-
+import "./contact.css"
 const page = () => {
   return (
     <>
-      <div className="contact-page pt-120 mb-120">
+      <div className="contact-page pt-120 contact-page-all-css">
         <div className="container">
-          <div className="row g-lg-4 gy-5">
-            <div className="col-lg-5">
-              <div className="single-contact mb-40">
+          <div className="row g-lg-4 gy-5 main-container-contact">
+            <div className="col-lg-4 left-side-contact-container">
+              <div className="single-contact">
                 <div className="title">
                   <h6>Phone</h6>
                 </div>
@@ -32,7 +32,8 @@ const page = () => {
                   </h6>
                 </div>
               </div>
-              <div className="single-contact mb-40">
+              <div className="single-contact 
+              ">
                 <div className="title">
                   <h6>Email Now</h6>
                 </div>
@@ -55,7 +56,7 @@ const page = () => {
                   </h6>
                 </div>
               </div>
-              <div className="single-contact mb-40">
+              <div className="single-contact">
                 <div className="title">
                   <h6>Location</h6>
                 </div>
@@ -123,7 +124,7 @@ const page = () => {
                 </div>
               </div>
             </div>
-            <div className="col-lg-7">
+            <div className="col-lg-6">
               <div className="contact-form-area">
                 <h3>Reach Us Anytime</h3>
                 <form>
@@ -172,55 +173,21 @@ const page = () => {
           </div>
         </div>
       </div>
-
-      <div className="contact-map">
+      
+<div className="contact-check-map">
+      {/* <div className="contact-map">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3649.5647631857846!2d90.36311167605992!3d23.83407118555764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c14c8682a473%3A0xa6c74743d52adb88!2sEgens%20Lab!5e0!3m2!1sen!2sbd!4v1700138349574!5m2!1sen!2sbd"
           allowFullScreen
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
         />
-        <div className="banner3-section">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12">
-                <div className="banner3-content">
-                  <h2>Join The Newsletter</h2>
-                  <p>To receive our best monthly deals</p>
-                  <form>
-                    <div className="from-inner">
-                      <input type="email" placeholder="Enter Your Gmail..." />
-                      <button type="submit" className="from-arrow">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width={18}
-                          height={17}
-                          viewBox="0 0 18 17"
-                        >
-                          <path
-                            d="M7 1L16 8.5M16 8.5L7 16M16 8.5H0.5"
-                            strokeWidth="1.5"
-                          />
-                        </svg>
-                      </button>
-                    </div>
-                  </form>
-                  <img
-                    src="/assets/img/home1/banner3-vector1.png"
-                    alt=""
-                    className="vector1"
-                  />
-                  <img
-                    src="/assets/img/home1/banner3-vector2.png"
-                    alt=""
-                    className="vector2"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+
+      </div> */}
+      <img src="assets/img/contact/map-photo.png" />
       </div>
+
+
     </>
   );
 };
