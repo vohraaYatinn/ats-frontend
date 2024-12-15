b1b690a4eb5a7ce46f5c8699e7b5dce93c210fc4
Rakshit Tiwari <59415104+tiwarirakshit@users.noreply.github.com>
2024-12-15 13:02:56 +0530

    feat: add Footer component to package-list page

--- src/app/package/package-list/page.js
+++ src/app/package/package-list/page.js
@@ -1,12 +1,14 @@
 "use client";
 
 import Header from "@/components/header/Header";
+import Footer from "@/components/footer/Footer3";
 import React from 'react'
 import '../../index2/style.css'
 import Home2BannerPackgeList from '@/components/banner/Home2BannerPackageList'
 import ExclusiveDealsCarousel from '../../../components/package-list/ExclusiveDealsCarousel'
 import PopularDestinations from '@/components/package-list/PopularDestinations';
 import PackagesList from '@/components/package-list/PackageList';
+import '../../../components/stylebook/Footer.css'
 
 const page = () => {
   return (
@@ -20,6 +22,8 @@ const page = () => {
         </div>
         <PopularDestinations/>
         <PackagesList/>
+        <hr/>
+        <Footer/>
     </>
   )
 }
