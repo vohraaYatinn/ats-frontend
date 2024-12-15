a1dbfc805d7f24c01842ebba2cea64ff4c9d7449
Rakshit Tiwari <59415104+tiwarirakshit@users.noreply.github.com>
2024-12-15 12:27:33 +0530

    Fixed Home Page

--- src/components/tourPackage/Home2ThrillingTour.jsx
+++ src/components/tourPackage/Home2ThrillingTour.jsx
@@ -8,18 +8,49 @@ import box3img from "../../../public/image/offer3.png"; // Update the path if ne
 import Slider from "react-slick";
 
 const Home2Destinationslide = () => {
+  
   const details = [
     {
-      img: box2img,
-      flightname: "Fixed Departure",
-      title: "Solo Travellers",
-      desc: "Social travel for 18-35 years old.",
+      img: box1img,
+      flightname: "International Flight",
+      title: "Huge savings on flight with trxvl.",
+      desc: "International flights starting @ just ₹4999",
     },
     {
       img: box2img,
-      flightname: "Fixed Departure",
-      title: "Solo Travellers 1",
-      desc: "Social travel for 18-35 years old.",
+      flightname: "International Flight",
+      title: "Enjoy upto 20% off on International Hotels",
+      desc: "International flights starting @ just ₹4999",
+    },
+    {
+      img: box3img,
+      flightname: "Domestic Flight",
+      title: "Explore the world with discounted rates.",
+      desc: "International flights starting @ just ₹4999",
+    },
+    {
+      img: box2img,
+      flightname: "International Flight",
+      title: "Explore the world with discounted rates.",
+      desc: "International flights starting @ just ₹4999",
+    },
+    {
+      img: box2img,
+      flightname: "International Flight",
+      title: "Explore the world with discounted rates.",
+      desc: "International flights starting @ just ₹4999",
+    },
+    {
+      img: box2img,
+      flightname: "International Flight",
+      title: "Explore the world with discounted rates.",
+      desc: "International flights starting @ just ₹4999",
+    },
+    {
+      img: box2img,
+      flightname: "International Flight",
+      title: "Explore the world with discounted rates.",
+      desc: "International flights starting @ just ₹4999",
     },
   ];
   const settings = useMemo(() => {
@@ -38,10 +69,12 @@ const Home2Destinationslide = () => {
         280: {
           slidesPerView: 1,
           spaceBetween: 100,
+
         },
         386: {
           slidesPerView: 1,
           spaceBetween: 40,
+
         },
         400: {
           slidesPerView: 1,
@@ -71,34 +104,43 @@ const Home2Destinationslide = () => {
           slidesPerView: 3,
           spaceBetween: 550,
         },
-        1400: {
-          slidesPerView: 3,
-          spaceBetween: 430,
-        },
-        1600: {
-          slidesPerView: 3,
-          spaceBetween: 300,
-        },
-        1700: {
-          slidesPerView: 3,
-          spaceBetween: 130,
-        },
-        1900: {
-          slidesPerView: 4,
-          spaceBetween: 510,
-        },
-        2000: {
-          slidesPerView: 4,
-          spaceBetween: 330,
-        },
-        2200: {
-          slidesPerView: 4,
-          spaceBetween: 250,
-        },
-        2400: {
-          slidesPerView: 4,
-          spaceBetween: 200,
-        },
+          1400: {
+            slidesPerView: 3,
+            spaceBetween: 430,
+
+
+          },
+          1600: {
+            slidesPerView: 3,
+            spaceBetween: 300,
+
+
+          },
+          1700: {
+            slidesPerView: 3,
+            spaceBetween: 130,
+
+          },
+          1900: {
+            slidesPerView: 4,
+            spaceBetween: 510,
+
+          },
+          2000: {
+            slidesPerView: 4,
+            spaceBetween: 330,
+
+          },
+          2200: {
+            slidesPerView: 4,
+            spaceBetween: 250,
+
+          },
+          2400: {
+            slidesPerView: 4,
+            spaceBetween: 200,
+
+          },
       },
     };
   }, []);
@@ -112,7 +154,7 @@ const Home2Destinationslide = () => {
     pauseOnFocus: false, // Prevents slider from pausing on focus
     pauseOnDotsHover: false, // Prevents slider from pausing when dots are hovered
     centerMode: true, // Enable center mode
-    centerPadding: "40px",
+    centerPadding: "40px", 
     arrows: false,
 
     responsive: [
@@ -122,7 +164,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "0px",
+          centerPadding: "0px", 
+
         },
       },
       {
@@ -131,7 +174,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "250px",
+          centerPadding: "250px", 
+
         },
       },
       {
@@ -140,7 +184,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "220px",
+          centerPadding: "220px", 
+
         },
       },
       {
@@ -149,7 +194,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "220px",
+          centerPadding: "220px", 
+
         },
       },
       {
@@ -158,7 +204,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "180px",
+          centerPadding: "180px", 
+
         },
       },
       {
@@ -167,7 +214,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "140px",
+          centerPadding: "140px", 
+
         },
       },
       {
@@ -176,7 +224,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "100px",
+          centerPadding: "100px", 
+
         },
       },
       {
@@ -185,7 +234,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "70px",
+          centerPadding: "70px", 
+
         },
       },
       {
@@ -194,7 +244,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "30px",
+          centerPadding: "30px", 
+
         },
       },
       {
@@ -203,7 +254,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "20px",
+          centerPadding: "20px", 
+
         },
       },
       {
@@ -212,7 +264,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "0px",
+          centerPadding: "0px", 
+
         },
       },
       {
@@ -221,7 +274,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "-20px",
+          centerPadding: "-20px", 
+
         },
       },
       {
@@ -230,7 +284,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "-50px",
+          centerPadding: "-50px", 
+
         },
       },
       {
@@ -239,7 +294,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "-90px",
+          centerPadding: "-90px", 
+
         },
       },
       {
@@ -248,7 +304,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "-90px",
+          centerPadding: "-90px", 
+
         },
       },
       {
@@ -257,7 +314,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "-90px",
+          centerPadding: "-90px", 
+
         },
       },
       {
@@ -266,7 +324,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "-120px",
+          centerPadding: "-120px", 
+
         },
       },
       {
@@ -275,7 +334,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "-120px",
+          centerPadding: "-120px", 
+
         },
       },
       {
@@ -284,7 +344,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "-130px",
+          centerPadding: "-130px", 
+
         },
       },
       {
@@ -293,7 +354,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "-80px",
+          centerPadding: "-80px", 
+
         },
       },
       {
@@ -302,7 +364,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "-110px",
+          centerPadding: "-110px", 
+
         },
       },
       {
@@ -311,7 +374,8 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "-90px",
+          centerPadding: "-90px", 
+
         },
       },
       {
@@ -320,17 +384,19 @@ const Home2Destinationslide = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "-110px",
+          centerPadding: "-110px", 
+
         },
       },
-
+  
       {
         breakpoint: 556,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
-          centerPadding: "60px",
+          centerPadding: "60px", 
+
         },
       },
       {
@@ -340,7 +406,8 @@ const Home2Destinationslide = () => {
           slidesToScroll: 1,
           infinite: true,
           centerMode: true,
-          centerPadding: "40px",
+          centerPadding: "40px", 
+
         },
       },
       {
@@ -350,7 +417,9 @@ const Home2Destinationslide = () => {
           slidesToScroll: 1,
           infinite: true,
           centerMode: true,
-          centerPadding: "0px",
+          centerPadding: "0px", 
+
+
         },
       },
       {
@@ -360,7 +429,9 @@ const Home2Destinationslide = () => {
           slidesToScroll: 1,
           infinite: true,
           centerMode: true,
-          centerPadding: "-40px",
+          centerPadding: "-40px", 
+
+
         },
       },
       {
@@ -370,60 +441,55 @@ const Home2Destinationslide = () => {
           slidesToScroll: 1,
           infinite: true,
           centerMode: true,
-          centerPadding: "0px",
+          centerPadding: "0px", 
+
+
         },
       },
+  
+  
     ],
   };
 
   return (
-    <div className="margintop checkmail exlusive-top-main">
-      <div className="heading-for-all rakshit-heading-for-all">
-        <h2>Exclusive Deals</h2>
-        <a href="#">HOT DEAL </a>
-      </div>
-      <div className="testimonial-card-slider-area">
-        <Slider {...sliderSettings} className="exclusive-offer-slick">
-          {details.map((detail, index) => (
-            <div className="swiper-slide" key={index}>
-              <div className="exclusive-deal">
-                <div className="row align-items-center">
-                  {/* Left Image */}
-                  <div className="div-image-exclusive col-6">
-                    <Image src={detail.img} alt="Exclusive Deal" />
-                  </div>
-
-                  {/* Right Content */}
-                  <div className="exclusive-content col-6">
-                    <span className="fixed-departure">{detail.flightname}</span>
-                    <h3 className="medium-head">{detail.title}</h3>
-                    <p className="lower-head">{detail.desc}</p>
-                    <button
-                      className="primary-btn1 home-button book-now rakshit-book-now-btn"
-                      data-bs-toggle="modal"
-                      data-bs-target="#user-login"
-                    >
-                      Book Now
-                      <svg
-                        width="18"
-                        height="17"
-                        viewBox="0 0 18 17"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M2.44301 12.4256C2.36781 12.2954 2.34745 12.166 2.38192 12.0376C2.41639 11.9092 2.4988 11.8073 2.62915 11.7321L13.4319 5.49505L8.88417 4.2784C8.73 4.23716 8.61532 4.15141 8.54012 4.02117C8.46493 3.89092 8.45271 3.75687 8.50347 3.61902C8.55424 3.48117 8.64479 3.37462 8.77514 3.29936C8.90549 3.22411 9.03835 3.19082 9.17371 3.1995L14.9548 4.74609C15.0764 4.80615 15.1747 4.9013 15.2499 5.03155C15.3251 5.16179 15.3584 5.29458 15.3496 5.42989L13.7984 11.2097C13.7383 11.3313 13.643 11.4297 13.5127 11.505C13.3823 11.5802 13.2448 11.6054 13.1 11.5804C12.9552 11.5555 12.8452 11.4779 12.77 11.3476C12.6949 11.2174 12.6779 11.0752 12.7193 10.921L13.9395 6.37423L3.13674 12.6112C3.00639 12.6865 2.87698 12.7069 2.74851 12.6726C2.62004 12.6382 2.51821 12.5559 2.44301 12.4256Z"
-                          fill="#F1871C"
-                        />
-                      </svg>
-                    </button>
-                  </div>
+    <div className="margintop checkmail exlusive-top-main" >
+  <div className="heading-for-all">
+  <h2>Exclusive Offers</h2>
+  <span>Select the offer that suits you best</span>
+  </div>
+  <div className="testimonial-card-slider-area">
+    
+                     <Slider {...sliderSettings} className="exclusive-offer-slick">
+        
+                     {details.map((detail, index) => (
+                
+                <div className="swiper-slide">
+           <div className="exclusive-deal row">
+                <div className="div-image-excluse col-6">
+                  <Image src={detail.img} alt="" />
                 </div>
-              </div>
-            </div>
-          ))}
+
+                <div className="≥≥≥≥. col-6">
+                  <p className="upper-head">{detail.flightname}</p>
+                  <h3 className="medium-head">{detail.title}</h3>
+                  <p className="lower-head">{detail.desc}</p>
+                  <button className="primary-btn1 two d-xl-flex d-none home-button book-now-thrill" data-bs-toggle="modal"
+            data-bs-target="#user-login">Book Now</button>
+                </div>
+           </div>
+                </div>
+                )
+              )}
+   
+      
+
         </Slider>
-      </div>
+            
+             
+         
+           
+       
+        </div>
     </div>
   );
 };
