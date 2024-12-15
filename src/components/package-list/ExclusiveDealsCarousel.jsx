eddca4bf6e4c329b936dfafcd63ac9724b422e24
Rakshit Tiwari <59415104+tiwarirakshit@users.noreply.github.com>
2024-12-15 16:37:42 +0530

    style: remove unnecessary blank line in ExclusiveDealsCarousel.jsx

--- src/components/package-list/ExclusiveDealsCarousel.jsx
+++ src/components/package-list/ExclusiveDealsCarousel.jsx
@@ -5,6 +5,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './ExclusiveDeals.css';
 
+
 const ExclusiveDeals = () => {
   const settings = {
     dots: false,
